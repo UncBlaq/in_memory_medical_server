d0ddf41b7b9a9ad1d9375093e84cc50b82a9eee1
black <michealadereti484@gmail.com>
2024-04-22 05:05:04 -0700

    Updated the process and cancel appointment endpoint

--- routers/appointment.py
+++ routers/appointment.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends, status
 from services.appointment import AppointmentService
 from schemas.appointment import AppointmentCreate, appointments, AppointmentStatus
 from schemas.doctor import doctors
+from routers.doctor import set_avalilability_status
 
 router = APIRouter()
 
@@ -34,45 +35,50 @@ def get_appointment_by_id(id : int):
 @router.put("/process_appointments/{id}", status_code=status.HTTP_202_ACCEPTED)
 def process_appointments(appointment_id : Annotated[int, Depends(AppointmentService.does_appointments_exist)]):
     #If an Appointment is already processed, then the appointed doctor will be free again
-    doctors_values = list(doctors.values())
-    for doctor in doctors_values:
-                if appointment_id == doctor.id:
-                    doctor.is_available = True
-#Status changes to completed
-    for appointment in appointments:
-        if appointment.id == appointment_id:
-            # Checks If appointment with provided id was already cancelled
-            if appointment.status == AppointmentStatus.canceled.value:
-                 return {
-                      "message": "This appointment was already canceled hence cannot be processed"
-                 }
-            #Else Process the appointment
-            appointment.status = AppointmentStatus.completed.value
+    doctor_values = list(doctors.values())
 
-            return {
-            "message" : "Appointment Processed Successfully"
-            } 
+    #Status changes to completed
+    for appointment in appointments:
+                if appointment.id == appointment_id:
+                    #Sets appointed doctor back to Available
+                    for doctor in doctor_values:
+                          if doctor == appointment.doctor:
+                                doctor.is_available = True
+
+                #checks if appointment was already cancelled
+                    if appointment.status == AppointmentStatus.canceled.value:
+                        return {
+                      "message": "This appointment was already canceled hence cannot be processed"
+                         }
+                     #Else Process the appointment and set to completed
+                    
+                    appointment.status = AppointmentStatus.completed.value
+
+                    return {
+                    "message" : "Appointment Processed Successfully"
+                    } 
 
 @router.put("/cancel_appointment/{id}", status_code=status.HTTP_202_ACCEPTED)
 def cancel_appointment(appointment_id : Annotated [int, Depends(AppointmentService.does_appointments_exist)]):
         #If an appointment is cancelled, the appointed doctor gets to be available again
         doctors_values = list(doctors.values())
-        for doctor in doctors_values:
-                if appointment_id == doctor.id:
-                    doctor.is_available = True
-    
-    #status changes to cancelled
         for appointment in appointments:
             if appointment.id == appointment_id:
+                #Sets appointed doctor back to Available
+                for doctor in doctors_values:  
+                    if doctor == appointment.doctor:
+                      doctor.is_available = True
+       
                      # Checks If appointment with provided id was already processed
                 if appointment.status == AppointmentStatus.completed.value:
+                            
                             return {
                       "message": "This appointment was already processed hence cannot be cancelled"
                  }
-                     
+                     #sets appointment to canceled
                 appointment.status = AppointmentStatus.canceled
                 return {
-                 "message" : "Appointment Cancelled Successfully"
+                    "message" : "Appointment Cancelled Successfully"
                     }
      
      
